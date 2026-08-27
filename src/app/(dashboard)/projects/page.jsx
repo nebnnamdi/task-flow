e2a14cd7aa98b4343e18879e616695d21fe1f1ff
Nnamdi <nebnnamdi@gmail.com>
2026-08-27 19:18:49 +0100

    chore: fixed styling errors in the header component

--- src/app/(dashboard)/projects/page.jsx
+++ src/app/(dashboard)/projects/page.jsx
@@ -2,9 +2,21 @@ import Link from "next/link";
 
 const ProjectPage = () => {
   return (
-    <div>
-      <p>Projects</p>
-      <Link href="/projects/new">New Project</Link>
+    <div className="flex flex-col">
+      <p className="text-2xl font-bold mb-4">Projects</p>
+
+      <section className="flex flex-col w-full bg-white p-4 gap-4">
+        <div>
+          <Link
+            href="/projects/new"
+            className="text-sm bg-purple-100 rounded-2xl py-2 px-4 transition-all duration-200 hover:-translate-y-1 hover:shadow-xl active:translate-y-0 active:shadow-md hover:bg-purple-200"
+          >
+            Create project
+          </Link>
+        </div>
+
+        <div></div>
+      </section>
     </div>
   );
 };
